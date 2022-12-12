45d3f1a98b236e0751c35ab8b92ef21ef51dfa0e
raul-gil-tiernoparla <raul.gildesagredomartin@tiernoparla.es>
2022-12-12 17:33:40 +0100

    añadidos atributos a Cuenta.java

--- src/main/java/daw/programacion/clases/Cuenta.java
+++ src/main/java/daw/programacion/clases/Cuenta.java
@@ -1,5 +1,12 @@
 package daw.programacion.clases;
 
 public class Cuenta {
+
+    // attr
+    private String id;
+    private String nombreCliente;
+    private double saldo = 0;
+    // constr
+    // meths
     
 } // Cuenta
