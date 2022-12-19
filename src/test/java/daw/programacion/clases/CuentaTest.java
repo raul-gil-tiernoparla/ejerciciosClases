c8f0f32bbf58b52bc61343929f3a99a8f28dbd2c
raul-gil-tiernoparla <raul.gildesagredomartin@tiernoparla.es>
2022-12-19 16:49:26 +0100

    creo metodos @before/After each en la clase CuentaTest.java

--- src/test/java/daw/programacion/clases/CuentaTest.java
+++ src/test/java/daw/programacion/clases/CuentaTest.java
@@ -1,8 +1,27 @@
 package daw.programacion.clases;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class CuentaTest {
+
+    Cuenta cuenta1;
+
+    @BeforeEach
+    void crearClaseCuenta1(){
+        String id = "Cd45gt";
+        String nombreCliente = "Ana Botín";
+        cuenta1 = new Cuenta(id,nombreCliente);
+    }
+
+    @AfterEach
+    void borrarClaseCuenta1(){
+        cuenta1 = null;
+    }
+
+    
     
 }
